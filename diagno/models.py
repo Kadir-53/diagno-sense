6c2d97a190b22614497b791751f719ee09c53242
juveriaulde <141655967+juveriaulde@users.noreply.github.com>
2024-02-15 18:26:02 +0000

    adjusted FAQ

--- diagno/models.py
+++ diagno/models.py
@@ -1,6 +1,19 @@
 from diagno import db
 
 
+class Users(db.Model):
+  __tablename__ = 'users'
+  id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
+  fname = db.Column(db.String(length=20), nullable=False)
+  lname = db.Column(db.String(length=20), nullable=False)
+  age = db.Column(db.Integer(), nullable=False)
+  gender = db.Column(db.String(length=20), nullable=False)
+  email = db.Column(db.String(length=50), nullable=False, uniqu=True)
+  password_hash = db.Column(db.String(length=60), nullable=False)
+
+# u1 = Users(fname='John', lname='Doe', age=20, gender='Male', email='john@gmail.com', password_hash='123456')
+
+
 class Item(db.Model):
   __tablename__ = "items"
   id = db.Column(db.Integer(), primary_key=True)
