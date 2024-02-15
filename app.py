2c31acc321904a0e98d934b5577ae34312d68f99
Kadir-53 <88156033+Kadir-53@users.noreply.github.com>
2024-02-15 10:25:26 +0000

    Created routes for all the webpages and did some formatting

--- app.py
+++ app.py
@@ -1,12 +1,39 @@
 from flask import Flask
+from flask.helpers import redirect
+from flask.templating import render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-  return "symptom.py"
+def index():
+  return render_template('index.html')
 
 
-if __name__ == '__main__':
+@app.route("/about")
+def about():
+  return render_template('about.html')
+
+
+@app.route("/contact")
+def contact():
+  return render_template('contact.html')
+
+
+@app.route("/login")
+def login():
+  return render_template('login.html')
+
+
+@app.route("/signup")
+def signup():
+  return render_template('signup.html')
+
+
+@app.route("/symptoms")
+def symptoms():
+  return render_template('symptoms.html')
+
+
+if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
