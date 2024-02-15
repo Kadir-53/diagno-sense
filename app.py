fe9d03ec478972b0b7e1a82989a31e1ff856fe24
juveriaulde <141655967+juveriaulde@users.noreply.github.com>
2024-02-15 11:25:42 +0000

    images added to all files

--- app.py
+++ app.py
@@ -3,9 +3,8 @@ from flask.helpers import redirect
 from flask.templating import render_template
 
 from flask import Flask
-app = Flask(__name__, static_url_path='/static')
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 
 @app.route("/")
