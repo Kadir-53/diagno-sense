c3f226f0fe8bac2b16e83aa368b4318b5f854188
Kadir-53 <88156033+Kadir-53@users.noreply.github.com>
2024-02-15 12:52:41 +0000

    created db model and config db

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask
 from flask.helpers import redirect
 from flask.templating import render_template
-
-from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, static_url_path='/static')
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///diagno.db'
+db = SQLAlchemy(app)
+
+
+class Item(db.Model):
+  id = db.Column(db.Integer(), primary_key=True)
+  name = db.Column(db.String(length=30), nullable=False, unique=True)
+  price = db.Column(db.Integer(), nullable=False)
+  barcode = db.Column(db.String(length=12), nullable=False, unique=True)
+  description = db.Column(db.String(length=1024), nullable=False, unique=True)
 
 
 @app.route("/")
